141353f459ff04d68c48772e050baef59d1a600f
Durakes <arnold.pajuelo@gmail.com>
2023-10-21 04:06:34 -0500

    Pull origin

---
+++ WebApp/Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using WebApp.Modelsñ
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+    }
+}
