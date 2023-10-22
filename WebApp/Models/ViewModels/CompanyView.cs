b2f099fa58642dc270ee8941acdb345f53535ec1
Durakes <arnold.pajuelo@gmail.com>
2023-10-22 01:08:27 -0500

    CompanyAPI Controller

---
+++ WebApp/Models/ViewModels/CompanyView.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp;
+
+public class CompanyView
+{
+    public string Name { get; set; } = null!;
+    public string? Address { get; set; }
+    public string? Email { get; set; }
+}
+
+public class CompanyDetails
+{
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+}
